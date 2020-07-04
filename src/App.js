0a0b68c236436306ce2fb3d8a4e8e7ea0b2fd5f2
Prabha Dodda <doddaprabha@gmail.com>
2020-07-04 17:04:46 +0800

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ class App extends Component {
   render() {
     return (
       <div>
+      {/* <div className="header">
         Phone Directory
+      </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br/>
+        <span>Phone</span>
+      </div> */}
+      <label htmlFor="name">Name: </label>
+      <input id="name" type="text" placeholder="Type here" defaultValue="Prabha"/>
       </div>
     );
   }
